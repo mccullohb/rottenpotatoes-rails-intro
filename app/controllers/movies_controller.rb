4268cae3ceff6c6e111e941ca890c1518a813589
EC2 Default User <ec2-user@ip-172-31-2-235.us-east-2.compute.internal>
2020-09-16 03:39:21 +0000

    Part 2 additional tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,21 +14,14 @@ class MoviesController < ApplicationController
     
     @all_ratings = Movie.order(:rating).pluck(:rating).uniq()
     
-    if params[:ratings]
-      @chosen_ratings = params[:ratings].keys
-    else
-      @chosen_ratings = @aggregate_ratings
-    end
-    
-    
-    # If specified sort, return movies sorted by specified column & highlighting specified column
     if params[:sort_type] == "title"
-      @movies = Movie.order(params[:sort_type]).where(:rating => @chosen_ratings)
+      @movies = Movie.order(params[:sort_type])
       @title_header = "hilite"
     elsif params[:sort_type] == "release_date"
-      @movies = Movie.order(params[:sort_type]).where(:rating => @chosen_ratings)
+      @movies = Movie.order(params[:sort_type])
       @release_date_header = "hilite"
     else
+      @chosen_ratings = params[:ratings].keys
       @movies = Movie.where(:rating => @chosen_ratings)
     end
   end
