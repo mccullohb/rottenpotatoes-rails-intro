bc27132080d5250f61e327401a9bcaa464ecc303
EC2 Default User <ec2-user@ip-172-31-2-235.us-east-2.compute.internal>
2020-09-16 02:06:16 +0000

    More tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,16 @@ class MoviesController < ApplicationController
 
   def index
     # If specified sort, return movies sorted by specified column
-    @movies = Movie.order(params[:sort])
+    if params[:sort]
+      @movies = Movie.order(params[:sort])
+    else
+      @movies = Movie.all
+    end
+    if params[:sort_type] == "title"
+      @title_header = "hilite"
+    elsif params[:sort_type] == "release_date"
+      @release_date_header = "hilite"
+    end
   end
 
   def new
